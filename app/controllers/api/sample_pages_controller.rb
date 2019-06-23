ce0616c55433e1f9c373ae04b404caf42819e9c5
Lauryn <laurynpearson@gmail.com>
2019-06-23 12:59:23 -0500

    add random fortune and random number to controller

--- app/controllers/api/sample_pages_controller.rb
+++ app/controllers/api/sample_pages_controller.rb
@@ -1,5 +1,8 @@
 class Api::SamplePagesController < ApplicationController
   def fortune_method
+    @fortune = ["You will obtain wealth.", "You will meet someone exciting.", "You love dogs."]
+    @fortune = @fortune.sample
+    @lucky_numbers = rand(1..60)
     render 'fortune.json.jb'
   end
 end
