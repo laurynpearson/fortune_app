8d9478cb25a892b890fc7276c0cea493b51362e6
Lauryn <laurynpearson@gmail.com>
2019-06-24 19:21:46 -0500

    update to 6 random numbers with random fortune

--- app/controllers/api/sample_pages_controller.rb
+++ app/controllers/api/sample_pages_controller.rb
@@ -1,8 +1,15 @@
 class Api::SamplePagesController < ApplicationController
   def fortune_method
-    @fortune = ["You will obtain wealth.", "You will meet someone exciting.", "You love dogs."]
-    @fortune = @fortune.sample
-    @lucky_numbers = rand(1..60)
+    fortune = ["You will obtain wealth.", "You will meet someone exciting.", "You love dogs."]
+    @fortune = fortune.sample
+
+    @lucky_numbers = []
+    6.times do
+      lottery_number = rand(1..60)
+      @lucky_numbers << lottery_number
+    end
     render 'fortune.json.jb'
   end
 end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
   namespace :api do
-  get "/fortune" => "sample_pages#fortune_method"
+    get "/fortune" => "sample_pages#fortune_method"
+
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
-end
+  end
 end
